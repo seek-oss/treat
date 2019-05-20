72c05100248dc9e6e9ba3c10a8cf014013a5c352
Matt Jones <mattjones701@gmail.com>
2019-05-21 06:57:56 +1000

    refcator: Move createContentHash to TS src

--- packages/treat/webpack-plugin/utils.js
+++ packages/treat/webpack-plugin/utils.js
@@ -1,20 +1,9 @@
-const crypto = require('crypto');
-
 const THEMED = 'THEMED';
 const LOCAL = 'LOCAL';
-
-const createContentHash = content => {
-  return crypto
-    .createHash('sha1')
-    .update(JSON.stringify(content))
-    .digest('base64')
-    .slice(0, 5);
-};
 
 const shortIdent = str => str.replace(/.*\//, '');
 
 module.exports = {
-  createContentHash,
   shortIdent,
   THEMED,
   LOCAL,

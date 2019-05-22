ecc673e01548d588a2bd38ef7cc599a1ded0da92
Matt Jones <mattjones701@gmail.com>
2019-05-22 17:26:31 +1000

    refactor: Move selector handling to own file

--- packages/treat/src/utils.ts
+++ packages/treat/src/utils.ts
@@ -1,6 +1,13 @@
 import { ThemeRef, ClassRef } from './types';
 
+export const themePlaceholder = '$';
+
+export const templateThemeClassRef = (classRef: string) =>
+  `${themePlaceholder}${classRef}`;
+
 export const makeThemedClassReference = (
   themeRef: ThemeRef,
   classRef: ClassRef,
 ) => `${classRef}${themeRef}`;
+
+export const convertToCssClass = (ref: string) => `.${ref}`;

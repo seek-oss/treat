fc24c7f6983d0dfb92f56127d38b8f659d19de78
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-05-27 12:26:00 +1000

    refactor: Fix error message indentation

--- packages/treat/src/validateSelector.ts
+++ packages/treat/src/validateSelector.ts
@@ -11,9 +11,11 @@ export const validateSelector = (selector: string) =>
   selector.split(',').map(selectorPart => {
     if (selectorPart.indexOf('&') === -1) {
       throw new Error(
-        dedent`Invalid selector: ${selector}
+        dedent`
+          Invalid selector: ${selector}
       
-        Selectors must target the ampersand character ('&'), which refers to the generated class name, e.g. '&:nth-child(2n)'`,
+          Selectors must target the ampersand character ('&'), which refers to the generated class name, e.g. '&:nth-child(2n)'
+        `,
       );
     }
 
@@ -37,15 +39,17 @@ export const validateSelector = (selector: string) =>
       )
     ) {
       throw new Error(
-        dedent`Invalid selector: ${selector}
+        dedent`
+          Invalid selector: ${selector}
       
-      Style selectors must end with the '&' character (along with any modifiers), e.g. ${'`${parent} &`'} or ${'`${parent} &:hover`'}.
-      
-      This is to ensure that each style block only affects the styling of a single class.
-      
-      If your selector is targeting another class, you should move it to the style definition for that class, e.g. given we have styles for 'parent' and 'child' elements, instead of adding a selector of ${'`& ${child}`'}) to 'parent', you should add ${'`${parent} &`'} to 'child').
-      
-      If your selector is targeting something global, use the 'globalStyle' function instead, e.g. if you wanted to write ${'`& h1`'}, you should instead write 'globalStyle(${'`${parent} h1`'}, { ... })'`,
+          Style selectors must end with the '&' character (along with any modifiers), e.g. ${'`${parent} &`'} or ${'`${parent} &:hover`'}.
+          
+          This is to ensure that each style block only affects the styling of a single class.
+          
+          If your selector is targeting another class, you should move it to the style definition for that class, e.g. given we have styles for 'parent' and 'child' elements, instead of adding a selector of ${'`& ${child}`'}) to 'parent', you should add ${'`${parent} &`'} to 'child').
+          
+          If your selector is targeting something global, use the 'globalStyle' function instead, e.g. if you wanted to write ${'`& h1`'}, you should instead write 'globalStyle(${'`${parent} h1`'}, { ... })'
+        `,
       );
     }
   });

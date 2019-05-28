1da4b890ca76523a2dafedbeecf9e3a9d842da7d
mattsjones <mattjones701@gmail.com>
2019-05-28 14:54:33 +1000

    feat: Allow themeable global styles (#19)

--- packages/treat/src/index.ts
+++ packages/treat/src/index.ts
@@ -2,4 +2,14 @@ export { createTheme, style, styleMap, css, globalStyle } from './builder';
 export { resolveStyles } from './resolveStyles';
 export { resolveClassName } from './resolveClassName';
 export { resolveClassNames, ResolveClassNamesArgs } from './resolveClassNames';
-export * from './types';
+
+// Backwards compat: Styles
+export {
+  Style,
+  Styles,
+  GlobalStyle,
+  CSSProperties,
+  ThemeRef,
+  ClassRef,
+  TreatModule,
+} from './types';

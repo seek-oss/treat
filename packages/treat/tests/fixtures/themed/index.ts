8f9b7db05ac32ccfcb7399be80ba5da238827465
mattsjones <mattjones701@gmail.com>
2019-07-24 16:39:51 +1000

    refactor: Remove deprecations (css, Styles, resolveClassNames) (#38)

--- packages/treat/tests/fixtures/themed/index.ts
+++ packages/treat/tests/fixtures/themed/index.ts
@@ -1,4 +1,5 @@
-import { resolveClassNames } from 'treat';
+import { resolveStyles } from 'treat';
+import classnames from 'classnames';
 import themeOne from './themeOne.treat';
 import themeTwo from './themeTwo.treat';
 
@@ -9,35 +10,45 @@ import stylesThree from './stylesThree.treat';
 const nodeOne = document.createElement('div');
 const nodeTwo = document.createElement('div');
 
+const themeOneStyles = resolveStyles(themeOne, {
+  stylesOne,
+  stylesTwo,
+  stylesThree,
+});
+
 nodeOne.setAttribute('id', 'theme1');
 nodeOne.setAttribute(
   'class',
-  resolveClassNames(
-    themeOne,
-    stylesOne.regularStyle,
-    stylesOne.themeStyle,
-    stylesTwo.regularStyle,
-    stylesTwo.themeStyle,
-    stylesThree.regularStyle,
-    stylesThree.themeStyle,
-    stylesThree.tree.border.small[0],
-    stylesThree.secondTree.textColor.primary,
+  classnames(
+    themeOneStyles.stylesOne.regularStyle,
+    themeOneStyles.stylesOne.themeStyle,
+    themeOneStyles.stylesTwo.regularStyle,
+    themeOneStyles.stylesTwo.themeStyle,
+    themeOneStyles.stylesThree.regularStyle,
+    themeOneStyles.stylesThree.themeStyle,
+    themeOneStyles.stylesThree.tree.border.small[0],
+    themeOneStyles.stylesThree.secondTree.textColor.primary,
   ),
 );
+
+const themeTwoStyles = resolveStyles(themeTwo, {
+  stylesOne,
+  stylesTwo,
+  stylesThree,
+});
 
 nodeTwo.setAttribute('id', 'theme2');
 nodeTwo.setAttribute(
   'class',
-  resolveClassNames(
-    themeTwo,
-    stylesOne.regularStyle,
-    stylesOne.themeStyle,
-    stylesTwo.regularStyle,
-    stylesTwo.themeStyle,
-    stylesThree.regularStyle,
-    stylesThree.themeStyle,
-    stylesThree.tree.border.large[0],
-    stylesThree.secondTree.textColor.primary,
+  classnames(
+    themeTwoStyles.stylesOne.regularStyle,
+    themeTwoStyles.stylesOne.themeStyle,
+    themeTwoStyles.stylesTwo.regularStyle,
+    themeTwoStyles.stylesTwo.themeStyle,
+    themeTwoStyles.stylesThree.regularStyle,
+    themeTwoStyles.stylesThree.themeStyle,
+    themeTwoStyles.stylesThree.tree.border.large[0],
+    themeTwoStyles.stylesThree.secondTree.textColor.primary,
   ),
 );
 

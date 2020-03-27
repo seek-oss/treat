2a29c56b848addb58d76c88d3db689564d1f2a5d
Kristóf Poduszló <kripod@protonmail.com>
2020-03-27 03:02:07 +0100

    test(nextjs-plugin): Add integration tests

---
+++ packages/next-plugin-treat/tests/styling.test.js
@@ -0,0 +1,99 @@
+import execa from 'execa';
+import * as path from 'path';
+import waitForLocalhost from 'wait-for-localhost';
+import getStyles from '../../../test-helpers/getStyles';
+import resolveBin from '../../../test-helpers/resolveBin';
+
+const nextjsServerPort = 9999;
+const nextjsBinaryPath = resolveBin('next', 'next');
+const nextjsFixturePath = path.resolve(__dirname, '../../next-plugin-treat-example');
+const nextjsExecArgs = {
+  shell: '/bin/bash', 
+  cwd: nextjsFixturePath,
+  env: {
+    NODE_ENV: 'development'
+  },
+  extendEnv: true
+};
+let nextjsProcess;
+
+async function navigateToServerWhenReady() {
+  await waitForLocalhost({port: nextjsServerPort});
+  await page.goto(`http://localhost:${nextjsServerPort}/`);
+}
+
+async function startDevServer() {
+  nextjsProcess = execa(`${nextjsBinaryPath} -p ${nextjsServerPort}`, nextjsExecArgs);
+  nextjsProcess.stdout.pipe(process.stdout);
+  nextjsProcess.stderr.pipe(process.stdout);
+  await navigateToServerWhenReady();
+}
+
+async function startProdServer() {
+  await execa(`${nextjsBinaryPath} build`, nextjsExecArgs);
+  nextjsProcess = execa(`${nextjsBinaryPath} start -p ${nextjsServerPort}`, nextjsExecArgs);
+  nextjsProcess.stdout.pipe(process.stdout);
+  nextjsProcess.stderr.pipe(process.stdout);
+  await navigateToServerWhenReady();
+}
+
+async function stopServer() {
+  nextjsProcess.kill();
+  await nextjsProcess;
+}
+
+describe('nextjs', () => {
+
+  describe('develop', () => {
+
+    beforeAll(() => startDevServer());
+    afterAll(() => stopServer());
+
+    test('it renders some global styles', async () => {
+      const styles = await getStyles(page, 'body');
+      expect(styles).toMatchSnapshot();
+    });
+    
+    test('it renders some button styles', async () => {
+      await page.waitForSelector('button');
+      const styles = await getStyles(page, 'button');
+      expect(styles).toMatchSnapshot();
+    });
+    
+    test('it loads and renders some span styles', async () => {
+      await page.waitForSelector('button');
+      await page.click('button');
+      await page.waitForSelector('span');
+      const styles = await getStyles(page, 'span');
+      expect(styles).toMatchSnapshot();
+    });
+
+  });
+  
+  describe('build', () => {
+    
+    beforeAll(() => startProdServer());
+    afterAll(() => stopServer());
+
+    test('it renders some global styles', async () => {
+      const styles = await getStyles(page, 'body');
+      expect(styles).toMatchSnapshot();
+    });
+    
+    test('it renders some button styles', async () => {
+      await page.waitForSelector('button');
+      const styles = await getStyles(page, 'button');
+      expect(styles).toMatchSnapshot();
+    });
+    
+    test('it loads and renders some span styles', async () => {
+      await page.waitForSelector('button');
+      await page.click('button');
+      await page.waitForSelector('span');
+      const styles = await getStyles(page, 'span');
+      expect(styles).toMatchSnapshot();
+    });
+
+  });
+
+});

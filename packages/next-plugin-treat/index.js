cece78eb2697b8bd42addc73d1ac365640acb993
Kristóf Poduszló <kripod@protonmail.com>
2020-03-27 14:27:11 +0100

    fix(nextjs-plugin): Extract CSS in production

--- packages/next-plugin-treat/index.js
+++ packages/next-plugin-treat/index.js
@@ -1,3 +1,4 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TreatPlugin = require('treat/webpack-plugin');
 
 module.exports = (pluginOptions = {}) => (nextConfig = {}) => {
@@ -5,9 +6,17 @@ module.exports = (pluginOptions = {}) => (nextConfig = {}) => {
     webpack(config, options) {
       config.plugins.push(
         new TreatPlugin(
-          Object.assign({}, pluginOptions, {
-            outputCSS: !options.isServer,
-          }),
+          Object.assign(
+            {},
+            pluginOptions,
+            { outputCSS: !options.isServer },
+            // Extract static CSS in production
+            // Logic adopted from https://github.com/zeit/next.js/blob/a81d7620a40b2006cb6726236402fc3f82f65ab8/packages/next/build/webpack/config/blocks/css/index.ts#L298-L320
+            !options.dev &&
+              !options.isServer && {
+                outputLoaders: [MiniCssExtractPlugin.loader],
+              },
+          ),
         ),
       );
 

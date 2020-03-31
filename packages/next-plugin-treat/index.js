b755786251c9e1d9aa9a020eb2ee3e5e94705a23
Kristóf Poduszló <kripod@protonmail.com>
2020-03-31 23:08:06 +0200

    fix(nextjs-plugin): SSR capability

--- packages/next-plugin-treat/index.js
+++ packages/next-plugin-treat/index.js
@@ -1,4 +1,6 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {
+  getClientStyleLoader,
+} = require('next/dist/build/webpack/config/blocks/css/loaders/client');
 const TreatPlugin = require('treat/webpack-plugin');
 
 module.exports = (pluginOptions = {}) => (nextConfig = {}) => {
@@ -6,17 +8,19 @@ module.exports = (pluginOptions = {}) => (nextConfig = {}) => {
     webpack(config, options) {
       config.plugins.push(
         new TreatPlugin(
-          Object.assign(
-            {},
-            pluginOptions,
-            { outputCSS: !options.isServer },
-            // Extract static CSS in production
-            // Logic adopted from https://github.com/zeit/next.js/blob/a81d7620a40b2006cb6726236402fc3f82f65ab8/packages/next/build/webpack/config/blocks/css/index.ts#L298-L320
-            !options.dev &&
-              !options.isServer && {
-                outputLoaders: [MiniCssExtractPlugin.loader],
-              },
-          ),
+          Object.assign({}, pluginOptions, {
+            outputCSS: !options.isServer,
+            outputLoaders: [
+              // Extract static CSS in production
+              // Logic adopted from https://github.com/zeit/next.js/blob/ee0081356d7ea166dfed4765f134730c11ecaecf/packages/next/build/webpack/config/blocks/css/loaders/global.ts#L13-L22
+              !options.isServer
+                ? getClientStyleLoader({
+                    isDevelopment: options.dev,
+                    assetPrefix: options.config.assetPrefix,
+                  })
+                : '',
+            ],
+          }),
         ),
       );
 

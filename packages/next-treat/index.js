d3de59a03d4a93f469b665a864dea8d17813dbd9
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2020-04-23 09:38:23 +1000

    refactor: Remove comment

--- packages/next-treat/index.js
+++ packages/next-treat/index.js
@@ -11,7 +11,6 @@ module.exports = (pluginOptions = {}) => (nextConfig = {}) => {
           Object.assign({}, pluginOptions, {
             outputCSS: !options.isServer,
             outputLoaders: [
-              // Extract static CSS in production
               // Logic adopted from https://github.com/zeit/next.js/blob/ee0081356d7ea166dfed4765f134730c11ecaecf/packages/next/build/webpack/config/blocks/css/loaders/global.ts#L13-L22
               !options.isServer
                 ? getClientStyleLoader({

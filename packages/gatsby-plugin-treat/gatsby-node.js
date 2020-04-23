e092a31ae97d4b9a58baae9b32a29eb4e7fb3657
Kristóf Poduszló <kripod@protonmail.com>
2020-04-23 12:58:58 +0200

    fix(gatsby-plugin): Output loading (again)

--- packages/gatsby-plugin-treat/gatsby-node.js
+++ packages/gatsby-plugin-treat/gatsby-node.js
@@ -13,16 +13,15 @@ exports.onCreateWebpackConfig = (
 ) => {
   if (stage === 'develop-html') return;
 
+  const isSSR = stage.includes('html');
   actions.setWebpackConfig({
     plugins: [
       new TreatPlugin({
         ...pluginOptions,
-        outputCSS: stage !== 'build-html',
+        outputCSS: !isSSR,
         outputLoaders: [
           // Logic adopted from https://github.com/gatsbyjs/gatsby/blob/73eb53b18a2e8e7cf451312d74f6c07ed3bf35bc/packages/gatsby/src/utils/webpack-utils.ts#L185-L193
-          !stage.includes('develop')
-            ? MiniCssExtractPlugin.loader
-            : require.resolve('style-loader'),
+          !isSSR ? MiniCssExtractPlugin.loader : 'style-loader',
         ],
       }),
     ],

5c639270a54e2236346e340abac691b2b466780a
Kristóf Poduszló <kripod@protonmail.com>
2020-04-23 11:51:46 +0200

    feat(gatsby-plugin): Fully customizable options

--- packages/gatsby-plugin-treat/gatsby-node.js
+++ packages/gatsby-plugin-treat/gatsby-node.js
@@ -10,30 +10,16 @@ exports.onCreateBabelConfig = ({ stage, actions }) => {
 exports.onCreateWebpackConfig = ({ stage, actions }, pluginOptions) => {
   if (stage === `develop-html`) return;
 
-  const defaultPluginOptions =
-    stage === `develop`
-      ? {
-          localIdentName: '[name]-[local]_[hash:base64:5]',
-          themeIdentName: '_[name]-[local]_[hash:base64:4]',
-        }
-      : {
-          localIdentName: '[hash:base64:5]',
-          themeIdentName: '[hash:base64:4]',
-        };
-  pluginOptions = {
-    ...defaultPluginOptions,
-    ...pluginOptions,
-  };
-
-  // Only the properties destructured below should be overridable from outside
-  const { localIdentName, themeIdentName } = pluginOptions;
-  pluginOptions = { localIdentName, themeIdentName };
-
-  if (stage === `build-javascript`) {
-    pluginOptions.outputLoaders = [MiniCssExtractPlugin.loader];
-  } else if (stage === `build-html`) {
-    pluginOptions.outputCSS = false;
-  }
-
-  actions.setWebpackConfig({ plugins: [new TreatPlugin(pluginOptions)] });
+  actions.setWebpackConfig({
+    plugins: [
+      new TreatPlugin({
+        ...pluginOptions,
+        outputCSS: stage !== `build-html`,
+        outputLoaders:
+          stage === `build-javascript`
+            ? [MiniCssExtractPlugin.loader]
+            : ['style-loader'],
+      }),
+    ],
+  });
 };

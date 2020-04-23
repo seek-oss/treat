2505a2ffe37a6d05e9270b399bc973a68bfe8194
Kristóf Poduszló <kripod@protonmail.com>
2020-04-23 14:04:41 +0200

    fix(gatsby-plugin): Make some options forced

--- packages/gatsby-plugin-treat/gatsby-node.js
+++ packages/gatsby-plugin-treat/gatsby-node.js
@@ -13,28 +13,28 @@ exports.onCreateWebpackConfig = (
 ) => {
   if (stage === 'develop-html') return;
 
-  const defaultPluginOptions =
-    stage === 'develop'
-      ? {
-          localIdentName: '[name]-[local]_[hash:base64:5]',
-          themeIdentName: '_[name]-[local]_[hash:base64:4]',
-        }
-      : {
-          localIdentName: '[hash:base64:5]',
-          themeIdentName: '[hash:base64:4]',
-        };
-
-  if (stage === 'build-javascript') {
-    pluginOptions.outputLoaders = [MiniCssExtractPlugin.loader];
-  } else if (stage === 'build-html') {
-    pluginOptions.outputCSS = false;
-  }
+  const isSSR = stage.includes('html');
+  const isDev = stage.includes('develop');
+  const defaultPluginOptions = isDev
+    ? {
+        localIdentName: '[name]-[local]_[hash:base64:5]',
+        themeIdentName: '_[name]-[local]_[hash:base64:4]',
+      }
+    : {
+        localIdentName: '[hash:base64:5]',
+        themeIdentName: '[hash:base64:4]',
+      };
 
   actions.setWebpackConfig({
     plugins: [
       new TreatPlugin({
         ...defaultPluginOptions,
         ...pluginOptions,
+        outputCSS: !isSSR,
+        outputLoaders: [
+          // Logic adopted from https://github.com/gatsbyjs/gatsby/blob/7bc6af46e5bd4cdde76be3fa4a857e00fc2e4635/packages/gatsby/src/utils/webpack-utils.ts#L185-L193
+          !isDev ? MiniCssExtractPlugin.loader : 'style-loader',
+        ],
       }),
     ],
   });

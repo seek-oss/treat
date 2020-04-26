1d440d9574b8fc8c1d86f5f3d0447a8d23f385e7
Matthew Davies <mdavies@mdtechnology.co.uk>
2020-04-26 19:24:52 +0100

    fix: Ensure consistent and valid content hashes

--- packages/treat/src/createContentHash.ts
+++ packages/treat/src/createContentHash.ts
@@ -1,10 +1,8 @@
-import crypto from 'crypto';
+import objectHash from 'object-hash';
 
 export const createContentHash = (content: any) => {
-  return crypto
-    .createHash('sha1')
-    .update(JSON.stringify(content))
-    .digest('base64')
+  return objectHash
+    .sha1(content)
     .slice(0, 5)
-    .replace(/^((-?[0-9])|--)/, '_$1');
+    .replace(/(^[0-9])/, '_$1');
 };

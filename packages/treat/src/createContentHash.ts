f8d9df22bf52bd2b60c1f950bb0d7cfd2143de17
Marais Rossouw <me@maraisr.com>
2020-06-15 22:16:00 +1000

    fix: Identifiers need to be stricter

--- packages/treat/src/createContentHash.ts
+++ packages/treat/src/createContentHash.ts
@@ -5,6 +5,9 @@ export const createContentHash = (content: any) => {
     .createHash('sha1')
     .update(JSON.stringify(content))
     .digest('base64')
+    // identifiers cannot start with double-dashes or numbers. Also if the first
+    // char is a hyphen and the 2nd isnt a letter or underscore, then replace
+    .replace(/^(((?:-{2,})?[0-9])|-[^a-z_])/i, '_$1')
+    .replace(/\+/g, '') // removes all +'s
     .slice(0, 5)
-    .replace(/^((-?[0-9])|--)/, '_$1');
 };

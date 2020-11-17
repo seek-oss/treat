02c178e423b2b64977fe02d310c1b158a1f61f94
Tom Sherman <the.tomsherman@gmail.com>
2020-11-17 21:43:24 +0000

    Add theme

--- packages/treat/tests/fixtures/gh-141/gh-141.treat.ts
+++ packages/treat/tests/fixtures/gh-141/gh-141.treat.ts
@@ -1,4 +1,9 @@
-import { style } from 'treat';
+import { style, createTheme } from 'treat';
+
+export const theme = createTheme({
+  name: 'yellow',
+  primaryColor: 'yellow',
+});
 
 export default {
   foo: style(() => ({

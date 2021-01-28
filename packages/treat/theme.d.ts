bc1abf102d8ec192aee25872827b252532718704
Matt Jones <mattjones701@gmail.com>
2021-01-29 09:11:39 +1100

    Format theme.d.ts

--- packages/treat/theme.d.ts
+++ packages/treat/theme.d.ts
@@ -2,6 +2,5 @@ type AnyIfEmpty<T extends object> = keyof T extends never ? any : T;
 
 declare module 'treat/theme' {
   export interface Theme {}
-  export type ThemeOrAny = AnyIfEmpty<Theme>
+  export type ThemeOrAny = AnyIfEmpty<Theme>;
 }
-
